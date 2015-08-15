b19b6bd17a2b119de0631893b518347102a4f589
Will Wharton <willmwharton@gmail.com>
2015-08-14 17:52:36 -0700

    add a few more items to the nav menu for logged in users

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -33,20 +33,37 @@ AppAsset::register($this);
             'class' => 'navbar-inverse navbar-fixed-top',
         ],
     ]);
+
+    $navItems = [
+        ['label' => 'Home', 'url' => ['/site/index']],
+        ['label' => 'About', 'url' => ['/site/about']],
+        ['label' => 'Contact', 'url' => ['/site/contact']],
+    ];
+
+    if ( Yii::$app->user->isGuest ) {
+        $navItems[] = [
+            'label' =>  'Login',
+            'url'   =>  ['/user/security/login']
+        ];
+    } else {
+        $navItems[] = [
+            'label' =>  Yii::$app->user->identity->username,
+            'url'   =>  ['/user'],
+        ];
+        $navItems[] = [
+            'label' =>  'settings',
+            'url'   =>  ['/user/settings'],
+        ];
+        $navItems[] = [
+            'label' =>  'Logout',
+            'url'   =>  ['/user/security/logout'],
+            'linkOptions'   => ['data-method' => 'post']
+        ];
+    }
+
     echo Nav::widget([
         'options' => ['class' => 'navbar-nav navbar-right'],
-        'items' => [
-            ['label' => 'Home', 'url' => ['/site/index']],
-            ['label' => 'About', 'url' => ['/site/about']],
-            ['label' => 'Contact', 'url' => ['/site/contact']],
-            Yii::$app->user->isGuest ?
-                ['label' => 'Login', 'url' => ['/user/security/login']] :
-                [
-                    'label' => 'Logout (' . Yii::$app->user->identity->username . ')',
-                    'url' => ['/user/security/logout'],
-                    'linkOptions' => ['data-method' => 'post']
-                ],
-        ],
+        'items' => $navItems,
     ]);
     NavBar::end();
     ?>
